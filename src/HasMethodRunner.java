26c50354e617904507b884a46b297cd186697396
こっぴー <@192.168.28.106>
2015-10-15 12:19:00 +0900

    Java_Syakyou オブジェクト指向

---
+++ src/HasMethodRunner.java
@@ -0,0 +1,22 @@
+
+class HasMethod {
+	
+	void printHello()
+	{
+		System.out.println( "Hello world" );
+	}
+}	
+
+class HasMethodRunner
+{
+	
+
+	public static void main(String[] args) 
+	{
+		// TODO 自動生成されたメソッド・スタブ
+		HasMethod ref = new HasMethod();
+		
+		ref.printHello();
+	}
+
+}
---
+++ src/SimpleClassRunner.java
@@ -0,0 +1,23 @@
+class SimpleClass 
+{
+	int data;
+}
+	class SimpleclaasRunner
+	{
+	
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		SimpleClass ref;
+		
+		ref = new SimpleClass();
+		
+		ref.data = 100;
+		
+		System.out.println( ref.data );
+		
+
+	}
+
+}
